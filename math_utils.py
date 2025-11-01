c2f54b494d6c9bb6db975de5c03f2b0ac7732139
sanahabib2005-eng <sanahabib2005@gmail.com>
2025-11-01 15:13:50 +0200

    Update math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -11,5 +11,5 @@ def find_mean(num1, num2, num3):
 
 def find_mean_std(num1, num2, num3):
     mean = find_mean(num1, num2, num3)
-    standard_deviation = ((num1-mean)**2 + (num2-mean)**2 + (num2-mean)**2)**0.5
+    standard_deviation = (((num1-mean)**2 + (num2-mean)**2 + (num2-mean)**2)/3)**0.5
     return (mean, standard_deviation)
