a47e120c79f997c53c34843f00ba393f76cbb27e
sanahabib2005-eng <sanahabib2005@gmail.com>
2025-11-03 14:23:15 +0200

    Update math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -1,10 +1,16 @@
 def find_max_number(num1, num2, num3):
-    if num1>num2 and num1>num3:
-       return num1
-    elif num2>num1 and num2>num3:
-       return num2
+    if (num1>num2) and (num1>num3):
+        return num1
+    elif (num1==num2) or (num1==num3):
+        return num1
+    elif (num2==num3):
+        return num2
+    elif (num1==num2) and (num1==num3):
+        return (num3)
+    elif (num2>num1) and (num2>num3):
+        return num2
     else:
-       return num3
+        return num3
 
 def find_mean(num1, num2, num3):
     return (num1 + num2 + num3)/3
@@ -13,4 +19,4 @@ def find_mean(num1, num2, num3):
 def find_mean_std(num1, num2, num3):
     mean = find_mean(num1, num2, num3)
     standard_deviation = ((((num1-mean)**2 + (num2-mean)**2 + (num3-mean)**2))/2)**0.5
-    return (standard_deviation , mean)
+    return (mean , standard_deviation)
