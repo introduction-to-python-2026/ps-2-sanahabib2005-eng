b9d616a7faf3999effdbc2410d9d3572ef35b054
sanahabib2005-eng <sanahabib2005@gmail.com>
2025-11-03 14:42:03 +0200

    Update math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -1,21 +1,25 @@
 def find_max_number(num1, num2, num3):
-    if num1 >= num2 and num1 >= num3:
-        return num1
-    elif num2 >= num1 and num2 >= num3:
-        return num2
+    if (num1>=num2) and (num2>=num3):
+     return (num1)
+    elif (num1>=num3) and (num3>=num2):
+      return (num1)
+    elif (num2>=num3) and (num3>=num1):
+      return(num2)
+    elif (num2>=num1) and (num1>=num3):
+      return(num2)
     else:
-        return num3
-
+      return(num3)
 
 def find_mean(num1, num2, num3):
     elements = (num1,num2,num3)
-    phase1 = (num1 + num2 + num3)
+    phase1 = (num1+num2+num3)
     phase2 = len(elements)
-    result = phase1 / phase2
+    result= (phase1/phase2)
     return result
 
-
 def find_mean_std(num1, num2, num3):
-    mean = find_mean(num1, num2, num3)
-    std = ((((num1-mean)**2 + (num2-mean)**2 + (num3-mean)**2))/2)**0.5
+    mean = find_mean(num1, num2, num3) 
+    element = (num1,num2,num3)
+    n_o_e = len(element)
+    std = ((((num1 - mean)*2) + ((num2 - mean)2) + ((num3 - mean)2))/n_o_e)*0.5
     return (mean,std)
